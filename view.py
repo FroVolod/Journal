35aa842238f87a78373b76076518834224f98396
FroVolod <frol_off@meta.ua>
2018-03-03 08:22:20 +0200

    перенес def co_author(self) из model во view

--- view.py
+++ view.py
@@ -10,21 +10,23 @@ from flask_security import current_user
 from articles.forms import New_Author
 
 
-def co_author(self):
+def co_author(author):
     print('Проверка входа в def co_author')
     if request.method == 'POST' and request.form['f_name']:
         print('Проверка reques.method:', request.method)
-        coauthor = Coauthor.query.filter(Coauthor.slug == self.slug).first()
+        author = request.form
+        coauthor = Coauthor.query.filter(Coauthor.f_name == author['f_name']).first()
         if coauthor:
-            coauthor.f_name = self.f_name
-            coauthor.l_name = self.l_name
-            coauthor.organization = self.organization
-            coauthor.email = self.email
-            coauthor.phone = self.phone
+            coauthor.f_name = author['f_name']
+            coauthor.l_name = author['l_name']
+            coauthor.organization = author['organization']
+            coauthor.email = author['email']
+            coauthor.phone = author['phone']
+            #coauthor.slug = author.slug
             db.session.merge(coauthor)
         else:
-            coauthor = Coauthor(f_name=self.f_name, l_name=self.l_name, organization=self.organization,
-                                email=self.email, phone=self.phone)
+            coauthor = Coauthor(f_name=author['f_name'], l_name=author['l_name'], organization=author['organization'],
+                                email=author['email'], phone=author['phone'])
             db.session.add(coauthor)
         db.session.commit()
 
@@ -119,7 +121,7 @@ class AuthorAdminView(AdminMixin, BaseModelView):
 
     def on_model_change(self, form, model, is_created):
         model.generate_slug()
-        model.co_author()
+        co_author(self)
         return super(BaseModelView, self).on_model_change(form, model, is_created)
 
 class JournalAdminView(AdminMixin, BaseModelView):

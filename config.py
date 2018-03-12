35ea89cc077f255bc080d09e65c0ba8cc67f38fa
Vlad Frolov <frolvlad@gmail.com>
2018-03-12 14:39:57 +0200

    Добавил возможность задания конфигурационных параметров через переменные окружения

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
+import os
+
+
 class Configuration(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/journal'
+    DEBUG = os.getenv('JOURNAL_DEBUG', True)
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{host}/{database}'.format(
+        user=os.getenv('JOURNAL_DB_USER', 'root'),
+        password=os.getenv('JOURNAL_DB_PASSWORD', '1234'),
+        host=os.getenv('JOURNAL_DB_HOST', 'localhost'),
+        database=os.getenv('JOURNAL_DB_NAME', 'journal'),
+    )
+    SECRET_KEY = os.getenv('JOURNAL_SECRET_KEY', 'keysecret')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'keysecret'
     SECURITY_PASSWORD_SALT = 'salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
